60350722fa9a3cda04dc0b4920ff3c9355e38ca2
yazan ali <yazanabuali2000@gmail.com>
2021-12-27 10:57:15 +0200

    add loader

--- client/src/Components/UserComponents/administrative.js
+++ client/src/Components/UserComponents/administrative.js
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { setTeachers, setCollegeAdministratives, setTeachersSearchList } from "../../state/actions/teachersActions";
 import SearchableTextField from '../PromotionCommitteeComponents/searchableTextField ';
 import axios from 'axios';
+import Loader from '../loader';
 
 function Administrative({ rank, college, section }) {
 
@@ -56,8 +57,8 @@ function Administrative({ rank, college, section }) {
 
         setIsLoading(true)
 
-        await axios.put("/administrative", data)
-            // await axios.put("http://localhost:5000/administrative", data)
+        // await axios.put("/administrative", data)
+        await axios.put("http://localhost:5000/administrative", data)
             .then(res => {
                 if (res.data.success) {
                     setCurrentAdministrative(newAdministrative)
@@ -71,52 +72,58 @@ function Administrative({ rank, college, section }) {
 
     return (
         <div className="administrative-info">
-            {rank === 1 && <h3 style={{ textAlign: "center" }}>رئيس قسم {section}</h3>}
-            {rank === 2 && <h3 style={{ textAlign: "center" }}>عميد كلية {college}</h3>}
-            {rank === 3 && <h3 style={{ textAlign: "center" }}>لجنة أمناء السر</h3>}
-            {rank === 4 && <h3 style={{ textAlign: "center" }}>لجنة التعين والترقية</h3>}
-            {rank === 5 && <h3 style={{ textAlign: "center" }}>رئاسة الجامعة</h3>}
-            <div style={{ display: "flex", flexDirection: "column" }}>
-                <div style={{
-                    width: currentAdministrative || newAdministrative && "50%",
-                    display: "flex",
-                    flexDirection: "column"
-                }}>
-                    {newAdministrative ?
-                        <>
-                            <p>الإسم : {newAdministrative.full_name}</p>
-                            <p>الرقم الوظيفي : {newAdministrative.teacher_id}</p>
-                            <p> الرتبة : {newAdministrative.rank}</p>
-                        </> : currentAdministrative && <>
-                            <p>الإسم : {currentAdministrative.full_name}</p>
-                            <p>الرقم الوظيفي : {currentAdministrative.teacher_id}</p>
-                            <p> الرتبة : {currentAdministrative.rank}</p>
-                        </>
-                    }
-                </div>
-                {showSearchField && <div style={{ width: "100%", marginTop: "2rem" }}>
-                    <SearchableTextField
-                        optionsList={searchList}
-                        placeholder={"اسم المستخدم"}
-                        selectOption={SelectNewAdministrative}
-                    />
-                </div>
-                }
-            </div>
+            {isLoading ? (
+                <Loader color={"gray"} size={2} />
+            ) : (
+                <>
+                    {rank === 1 && <h3 style={{ textAlign: "center" }}>رئيس قسم {section}</h3>}
+                    {rank === 2 && <h3 style={{ textAlign: "center" }}>عميد كلية {college}</h3>}
+                    {rank === 3 && <h3 style={{ textAlign: "center" }}>لجنة أمناء السر</h3>}
+                    {rank === 4 && <h3 style={{ textAlign: "center" }}>لجنة التعين والترقية</h3>}
+                    {rank === 5 && <h3 style={{ textAlign: "center" }}>رئاسة الجامعة</h3>}
+                    <div style={{ display: "flex", flexDirection: "column" }}>
+                        <div style={{
+                            width: currentAdministrative || newAdministrative && "50%",
+                            display: "flex",
+                            flexDirection: "column"
+                        }}>
+                            {newAdministrative ?
+                                <>
+                                    <p>الإسم : {newAdministrative.full_name}</p>
+                                    <p>الرقم الوظيفي : {newAdministrative.teacher_id}</p>
+                                    <p> الرتبة : {newAdministrative.rank}</p>
+                                </> : currentAdministrative && <>
+                                    <p>الإسم : {currentAdministrative.full_name}</p>
+                                    <p>الرقم الوظيفي : {currentAdministrative.teacher_id}</p>
+                                    <p> الرتبة : {currentAdministrative.rank}</p>
+                                </>
+                            }
+                        </div>
+                        {showSearchField && <div style={{ width: "100%", marginTop: "2rem" }}>
+                            <SearchableTextField
+                                optionsList={searchList}
+                                placeholder={"اسم المستخدم"}
+                                selectOption={SelectNewAdministrative}
+                            />
+                        </div>
+                        }
+                    </div>
 
-            <div style={{ marginTop: "2rem" }}>
-                <Button onClick={toggleSearchField} style={{ backgroundColor: "#098D9C" }} primary>
-                    {`${showSearchField ? "إلغاء" : "تغيير"}`}
-                </Button>
-                {newAdministrative &&
-                    <Button
-                        loading={isLoading}
-                        disabled={isLoading}
-                        onClick={handleSubmit}
-                        style={{ backgroundColor: "#098D9C" }}
-                        primary>حفظ
-                    </Button>}
-            </div>
+                    <div style={{ marginTop: "2rem" }}>
+                        <Button onClick={toggleSearchField} style={{ backgroundColor: "#098D9C" }} primary>
+                            {`${showSearchField ? "إلغاء" : "تغيير"}`}
+                        </Button>
+                        {newAdministrative &&
+                            <Button
+                                loading={isLoading}
+                                disabled={isLoading}
+                                onClick={handleSubmit}
+                                style={{ backgroundColor: "#098D9C" }}
+                                primary>حفظ
+                            </Button>}
+                    </div>
+                </>
+            )}
         </div>
     )
 }
